0bdcc48e95ef58eb557bbf0ef44111b922616c45
jbyall <jpb5002@gmail.com>
2017-12-04 20:28:52 -0700

    Fixed windows authentication settings.

--- SIS/SIS.Web/Controllers/HomeController.cs
+++ SIS/SIS.Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace SIS.Web.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
