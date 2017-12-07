2f445f5ff336031724bbdc4e0ed5a601ff7adacf
jbyall <jpb5002@gmail.com>
2017-12-06 18:57:50 -0700

    Added currency format.

--- SIS/SIS.Web/Controllers/HomeController.cs
+++ SIS/SIS.Web/Controllers/HomeController.cs
@@ -13,21 +13,21 @@ namespace SIS.Web.Controllers
     {
         public ActionResult Index()
         {
-            // Get the logged-in user's WindowsIdentity
-            WindowsIdentity identity = (WindowsIdentity)User.Identity;
+            //// Get the logged-in user's WindowsIdentity
+            //WindowsIdentity identity = (WindowsIdentity)User.Identity;
 
-            // Get a list of the user's groups
-            var groups = identity.Groups.ToList();
+            //// Get a list of the user's groups
+            //var groups = identity.Groups.ToList();
             
-            // Loop over each of the user's groups and find the NT group name
-            // Note - by default, the group is displayed as an SID
-            foreach (var item in identity.Groups.ToList())
-            {
-                var groupName = item.Translate(typeof(NTAccount)).Value;
-            }
+            //// Loop over each of the user's groups and find the NT group name
+            //// Note - by default, the group is displayed as an SID
+            //foreach (var item in identity.Groups.ToList())
+            //{
+            //    var groupName = item.Translate(typeof(NTAccount)).Value;
+            //}
 
-            // To test if the user is in a specific group
-            //var isInGroup = User.IsInRole(@"domain\group");
+            //// To test if the user is in a specific group
+            ////var isInGroup = User.IsInRole(@"domain\group");
 
             return View();
         }
